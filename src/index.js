1b8ea671c1b811fe6a48c479c2273686c569bfd2
Raghib Aftab <raghib@raghibs-air.lan>
2021-06-03 03:25:32 -0500

    updated query util.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const chalk = require('chalk')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
-const queryRouter = require('./routers/query')
+const cardRouter = require('./routers/card')
 
 // Init express 
 const app = express()
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouter)
-app.use(queryRouter)
+app.use(cardRouter)
 
 // Listen for connection
 app.listen(port, () => {
